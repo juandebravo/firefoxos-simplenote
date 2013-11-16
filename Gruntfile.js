f5fde090f98255e3feb7571a9b79aac88bb75eae
juandebravo <juandebravo@gmail.com>
2013-11-17 00:51:13 +0100

    include copy:diet grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-open');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.initConfig({
     connect: {
@@ -17,9 +18,37 @@ module.exports = function (grunt) {
     },
 
     watch: {
-    }
+    },
 
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '.',
+          dest: 'dist',
+          src: [
+            /** COMPONENTS - Extracted from main.js config **/
+            'bower_components/requirejs/require.js',
+            'bower_components/db/index.js',
+            'bower_components/js-base64/base64.min.js',
+            'bower_components/markdown/lib/markdown.js',
+            'bower_components/underscore/underscore-min.js',
+            'bower_components/when/when.js',
+            'bower_components/zepto/zepto.min.js',
+            /** END OF COMPONENTS **/
+            'shared/**/*',
+            'style/*.css',
+            'style/icons/*.{png,jpeg}',
+            'scripts/**/*.js',
+            'index.html',
+            'manifest.webapp'
+          ]
+        }]
+      }
+    },
   });
+
   grunt.registerTask('server', function () {
     grunt.task.run([
       'connect:server',
@@ -27,4 +56,9 @@ module.exports = function (grunt) {
       'watch'
     ]);
   });
+
+  grunt.registerTask('dist', [
+    'copy:build',
+  ]);
+
 };
